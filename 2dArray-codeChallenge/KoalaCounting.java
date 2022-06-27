7b50c308422575dcd1d2b02c328f22c7886f4749
Caleb M. McLaren <kbmclaren@gmail.com>
2022-06-27 18:14:04 -0400

    overload getTotal to accomidate 2d array

--- 2dArray-codeChallenge/KoalaCounting.java
+++ 2dArray-codeChallenge/KoalaCounting.java
@@ -2,8 +2,19 @@ public class KoalaCounting {
 
     public static int getAvg( int[] arr ){
         int total = KoalaCounting.getTotal(arr); 
-        
         return total / arr.length; 
+    }
+
+    public static int getTotal( int[][] arr2D){
+        int total = 0; 
+
+        for( int[] day: arr2D ){
+            for( int eachCount: day){
+                total += eachCount; 
+            }
+        }
+
+        return total; 
     }
 
     public static int getTotal( int[] arr ){
@@ -16,10 +27,14 @@ public class KoalaCounting {
         return total; 
     }
     public static void main( String[] args ){
-        int[] dayOne = {17, 13, 19, 22};
+        /* int[] dayOne = {17, 13, 19, 22};
         System.out.println(getTotal(dayOne));
         System.out.println( "Expected count: 71"); 
         System.out.println(getAvg(dayOne));
-        System.out.println( "Expected average: 17");
+        System.out.println( "Expected average: 17"); */
+
+        int[][] koalaSpottings = {{17, 13, 19, 22}, {12, 18, 25, 20}, {15, 18, 21, 24}, {19, 23, 23, 22}, {18, 20, 21, 26}};
+        System.out.println(getTotal(koalaSpottings));
+        System.out.println("Total expected: 396");
     }
 }
