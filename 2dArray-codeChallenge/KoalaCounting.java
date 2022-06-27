7c68ac75e184787da1d3983298e683d95d505cea
Caleb M. McLaren <kbmclaren@gmail.com>
2022-06-27 17:52:03 -0400

    add getAvg

--- 2dArray-codeChallenge/KoalaCounting.java
+++ 2dArray-codeChallenge/KoalaCounting.java
@@ -1,5 +1,11 @@
 public class KoalaCounting {
 
+    public static int getAvg( int[] arr ){
+        int total = KoalaCounting.getTotal(arr); 
+        
+        return total / arr.length; 
+    }
+
     public static int getTotal( int[] arr ){
         int total = 0; 
 
@@ -13,5 +19,7 @@ public class KoalaCounting {
         int[] dayOne = {17, 13, 19, 22};
         System.out.println(getTotal(dayOne));
         System.out.println( "Expected count: 71"); 
+        System.out.println(getAvg(dayOne));
+        System.out.println( "Expected average: 17");
     }
 }
